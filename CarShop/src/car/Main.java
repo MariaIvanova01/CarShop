b72c1244f48b9a673029fdc0deb052910d731720
MariaIvanova01 <mimi200801@abv.bg>
2022-07-28 21:09:39 +0300

    Build hierarchy from classes and interfaces

--- CarShop/src/car/Main.java
+++ CarShop/src/car/Main.java
@@ -3,6 +3,13 @@ package car;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Car seat = new Seat("Leon", "gray", 110, "Spain");
+
+        System.out.println(String.format(
+                "%s is %s color and have %s horse power",
+                seat.getModel(),
+                seat.getColor(),
+                seat.getHorsePower()));
+        System.out.println(seat);
     }
 }
